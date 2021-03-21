af607601d294f1db66daff786ddb3b6ff7eb6e4d
Jonathan Wilding <jonathan.r.wilding@gmail.com>
2021-03-20 19:51:43 -0600

    adding data structure

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,30 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+let data = {
+  netid: 'jrw265',
+  dataEntered: [
+    {
+      id: 1,
+      probl: "This app is not completely written yet.",
+      name: "Emma"
+    }
+  ],
+  getDataEntered() {
+    return this.dataEntered;
+  },
+  addData(name, problem) {
+    this.tickets.push({
+      id: this.currentID,
+      problem: problem,
+      name: name
+    });
+    this.currentID += 1;
+  }
+};
+
 new Vue({
   router,
+  data: data,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
